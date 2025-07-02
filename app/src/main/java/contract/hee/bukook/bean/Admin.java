75b0ac7defdc5dd9cf25beb5a24cdbe66473e14b
pys890102 <pys890102@bkwinners.com>
2025-07-02 17:20:38 +0900

    도메인 변경

--- app/src/main/java/contract/hee/bukook/bean/Admin.java
+++ app/src/main/java/contract/hee/bukook/bean/Admin.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 public class Admin {
     //public static String sic_url ="sicapp.kr"; //real
-    public static String sic_url ="https://winnerssign.bkwinners.kr"; //real
+    public static String sic_url ="https://winnerssign.ghpayments.kr"; //real
     //public static String sic_url ="http://211.44.250.131"; //real
     //public static String sic_url ="http://112.217.198.67"; //ftp
 }
